1ed388f4eedf87c9a8196c438070635252d0c792
Matthew Henderson <hendersonmpa@gmail.com>
2018-04-07 17:59:06 -0400

    prelim refinement

--- rank.r
+++ rank.r
@@ -3,19 +3,23 @@ library("tidyverse")
 
 ## 2018 Data source https://www.hockey-reference.com/leagues/NHL_2018_skaters.html
 
-points <- read.csv("stats_raw.csv", header = TRUE, stringsAsFactors = FALSE)
+points <- read.csv("stats_2018.csv", header = TRUE, stringsAsFactors = FALSE)
 
 ## Create team weight
-percent <- read.csv("odds.csv", header = TRUE, stringsAsFactors = FALSE)
+percent <- read.csv("odds_2018.csv", header = TRUE, stringsAsFactors = FALSE)
 
-data <- merge(x = points, y = percent, by = "team", all = TRUE)
+data <- merge(x = points, y = percent, by.x = "Tm", by.y = "team", all = FALSE)
 
 data %>%
-    na.omit(.) %>%
-    transform(index = percent * ppg) %>%
-    filter(gp > 30) %>%
+#    na.omit(.) %>%
+    transform(index = (one * 0.5 * PTS) + (final * PTS)) %>%
+    filter(GP > 30) %>%
     arrange(desc(index)) %>%
-    select(team, name, gp, index)
+    select(Tm, Player, PTS, GP, index) %>%
+    write.table(file = "picklist.csv", append = FALSE, quote = FALSE, sep = "\t",
+            eol = "\n", na = "NA", dec = ".", row.names = TRUE,
+            col.names = TRUE, qmethod = c("escape", "double"),
+            fileEncoding = "")
 
 
 
