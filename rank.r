22cbd7fc950b3b8def8cb742fde5314f9e255a11
Matthew Henderson <hendersonmpa@gmail.com>
2018-04-08 14:48:46 -0400

    A bit better

--- rank.r
+++ rank.r
@@ -10,13 +10,26 @@ percent <- read.csv("odds_2018.csv", header = TRUE, stringsAsFactors = FALSE)
 
 data <- merge(x = points, y = percent, by.x = "Tm", by.y = "team", all = FALSE)
 
+contribution <- function(pquarter,psemi,pfinal, PTS, GP){
+    ppg <- PTS / GP
+    pps <- ppg * 6 ## points per series, used in first round
+    quarter <- pquarter * pps
+    semi <- pquarter * psemi * pps
+    final <- pquarter * psemi * pfinal *  pps
+    return(round(x = pps + quarter + semi + final, digits = 2)) 
+}
+
+
+
+
 data %>%
-#    na.omit(.) %>%
-    transform(index = (one * 0.5 * PTS) + (final * PTS)) %>%
+##    na.omit(.) %>%
+##    transform(index = contribution(pquarter, psemi, pfinal,PTS,GP)) %>%
+    transform(index = PTS) %>%
     filter(GP > 30) %>%
     arrange(desc(index)) %>%
     select(Tm, Player, PTS, GP, index) %>%
-    write.table(file = "picklist.csv", append = FALSE, quote = FALSE, sep = "\t",
+    write.table(file = "picklistPTS.csv", append = FALSE, quote = FALSE, sep = "\t",
             eol = "\n", na = "NA", dec = ".", row.names = TRUE,
             col.names = TRUE, qmethod = c("escape", "double"),
             fileEncoding = "")
